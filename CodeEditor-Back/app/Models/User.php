b70fe4f82d44e1ad08d5548705ce020e0ce7fafb
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-25 17:46:19 +0300

    Added User model

--- CodeEditor-Back/app/Models/User.php
+++ CodeEditor-Back/app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function code_submission()
+    {
+        $this->hasMany(Code_submissin::class);
+    }
 }
