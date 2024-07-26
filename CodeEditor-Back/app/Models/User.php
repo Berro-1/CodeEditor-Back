64643a906122e0823fe3be267b248d1bd8be1877
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-26 14:32:12 +0300

    Fixed bug

--- CodeEditor-Back/app/Models/User.php
+++ CodeEditor-Back/app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
     ];
     public function code_submission()
     {
-        $this->hasMany(Codesubmissin::class);
+        $this->hasMany(Codesubmission::class);
     }
     public function chat()
     {
