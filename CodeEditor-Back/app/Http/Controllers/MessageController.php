7ef6f64772f9f93753435c78938728d171140402
Berro-1 <hussienberro1@gmail.com>
2024-07-27 03:12:38 +0300

    added function to create a message for a certain chat

--- CodeEditor-Back/app/Http/Controllers/MessageController.php
+++ CodeEditor-Back/app/Http/Controllers/MessageController.php
@@ -11,9 +11,27 @@ class MessageController extends Controller
     public function get(Request $request, $chat_id)
     {
 
-        $chat = Chat::find($chat_id);
         $messages = Message::where('chat_id', $chat_id)->get();
         return response()->json($messages, 200);
     }
-   
+    public function createMessage(Request $request, $chat_id)
+    {
+        $request->validate([
+            'sender_id' => 'required|exists:users,id',
+            'message' => 'required|string',
+        ]);
+        $chat = Chat::find($chat_id);
+        if (!$chat) {
+            return response()->json(['error' => 'Chat not found'], 404);
+        }
+        $message = new Message();
+        $message->chat_id = $chat_id;
+        $message->sender_id = $request->sender_id;
+        $message->message = $request->message;
+        $message->save();
+
+        return response()->json([
+            'message' => $message,
+        ], 201);
+    }
 }
