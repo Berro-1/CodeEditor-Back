9c940cd72756c266224980272cb7862310aadacb
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-27 15:20:56 +0300

    Added routes

--- CodeEditor-Back/routes/api.php
+++ CodeEditor-Back/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CodesubmissionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+
 Route::prefix('chat')->group(function () {
     Route::post("/createChat", [\App\Http\Controllers\ChatController::class, "createChat"]);
     Route::get("/", [App\Http\Controllers\ChatController::class, "getAllChats"]);
