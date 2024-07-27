7e3225dc22f82878b735147684d03a7bc243bf24
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-27 08:51:36 +0300

    Added user controller

---
+++ CodeEditor-Back/app/Http/Controllers/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+  public function readAllUsers()
+    {
+        $users = User::get();
+        return response()->json([$users], 200);
+    }
+    public function readUser($id){
+        $user=User::find($id);
+        return response()->json([
+           $user
+        ],200);
+    }
+
+    // public function createUser(Request $request){
+        
+    //     $validated_data = $request->validate([
+    //         "id" => "required|exists:user,id|numeric",
+    //         "name" => "required|string|max:255",
+    //         "email" => "required|string|email|max:255|unique:user,email",
+    //         "password" => "required|string|min:6|confirmed"
+
+
+    //     ]);
+        
+    //     $user =User::create($validated_data);
+    //     return response()->json([
+    //         "user" => $user
+    //     ], 201);
+    // }
+    public function createUser(Request $request)
+{
+    // Validate the incoming request
+    $validated_data = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users,email',
+        'password' => 'required|string|min:6',
+    ]);
+
+    // Encrypt the password
+    $validated_data['password'] = bcrypt($validated_data['password']);
+
+    // Create the user
+    $user = User::create($validated_data);
+
+    // Return the created user
+    return response()->json([
+       $user
+    ], 201);
+}
+    
+    //////
+    public function updateUser(Request $request ,$id){
+        $user=User::find($id);
+
+        if($user){
+            $validated_data=$request->validate([
+                "name"=>"required|string|max:255",
+                 "email"=>"required|string|email|max:255|unique:user,email",
+                 "password"=>"required|string|min:8|confirmed"
+            ]);
+        }
+        $user->update($validated_data);
+        return response()->json([
+            "message"=>"User updated successfully"
+        ],204);
+
+    }
+
+
+    public function deleteUser($id){
+        $user=User::find($id);
+        $user->delete();
+        return response()->json([
+            "message" => "User deleted successfully"
+        ],204);
+
+    }
+}
