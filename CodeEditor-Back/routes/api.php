ed16f694e81a778f6a765f11891767e27dda265e
Berro-1 <hussienberro1@gmail.com>
2024-07-27 03:08:37 +0300

    Added routes for messages and chats

--- CodeEditor-Back/routes/api.php
+++ CodeEditor-Back/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('chat')->group(function () {
+    Route::post("/createChat", [\App\Http\Controllers\ChatController::class, "createChat"]);
+    Route::get("/", [App\Http\Controllers\ChatController::class, "getAllChats"]);
+});
+
+Route::prefix('message')->group(function () {
+    Route::post("/createMessage/{chat_id}", [\App\Http\Controllers\MessageController::class, "createMessage"]);
+    Route::get("/get/{chat_id}", [App\Http\Controllers\MessageController::class, "get"]);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
