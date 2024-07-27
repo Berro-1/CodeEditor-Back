f3dd18b08e47cd4c8e41389923d1d5ee572e1859
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2024-07-27 08:51:53 +0300

    Added routes

--- CodeEditor-Back/routes/api.php
+++ CodeEditor-Back/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CodesubmissionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group([
+    // "middleware" => "authenticate",
+    "prefix" => "users",
+    "controller" => UserController::class
+], function () {
+    Route::get('/', [UserController::class, 'readAllUsers']);    
+    Route::get('/{id}',  'readUser');
+    Route::post('/', 'createUser');
+    Route::delete('/{id}',  'deleteUser');
+    Route::put('/{id}',  'updateUser');
+    // Route::get('/menus/{id}', 'restaurantMenus');
 });
+
+
+
+Route::group([
+    // "middleware" => "authenticate",
+    "prefix" => "code",
+    "controller" => CodesubmissionController::class
+], function () {
+    Route::get('/', [CodesubmissionController::class, 'readAllCodes']);    
+    Route::get('/{id}',  'readCode');
+    // Route::post('/', 'createUser');
+    // Route::delete('/{id}',  'deleteUser');
+    // Route::put('/{id}',  'updateUser');
+    // Route::get('/menus/{id}', 'restaurantMenus');
+});
+
+
+
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
